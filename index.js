63840f2895d00a2ce1a0abd4172e8d1aa3d2185a
oscar leon <first.liberal.thinker@gmail.com>
2018-08-27 23:15:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ function sayHiToGrandma(string){
     if (string === string.toLowerCase()){
     return "I can't hear you!"	    
   }else if (string === uppercase){	  }else if (string === string.toUpperCase()){
-    return "YES INDEED!"	    return "YES INDEED!"
+    	    return "YES INDEED!"
   }else if (string === "I love you, Grandma."){	  }else if (string === "I love you, Grandma."){
-    return "I love you, too."	    return "I love you, too."
-}
+    return "I love you, too."}}
